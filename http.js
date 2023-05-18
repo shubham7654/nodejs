9351fdad67bc14afc3dc40ef0dfb439b3f03d27e
Shubham <shubham@BS-599.local>
2023-05-18 16:59:10 +0530

    Updated http.js

--- http.js
+++ http.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
+    console.log("Loaded!");
     res.end("Welcome to the homepage!!!");
   }
   if (req.url === "/about") {
